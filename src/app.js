ceab573facf1e2f57a1416c60cf5f2dc64d94569
anasraza07 <ar470684@gmail.com>
2024-08-08 06:21:28 +0500

    user and video model added

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(
   })
 );
 
+// configuration or middlewares
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
